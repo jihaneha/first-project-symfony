6edeabbeca35f8c2472aa44b543150b9b3621c05
jihaneha <ghamidou800@gmail.com>
2022-08-01 12:03:57 +0200

    modif

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.scss";
 
-// start the Stimulus application
-import "./bootstrap";
+// app.js
+
+const $ = require("jquery");
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require("bootstrap");
+
+console.log("ca marche");
